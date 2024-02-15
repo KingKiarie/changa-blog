c8bbb993fd3ddd22416ab91dff85108358717142
KingKiarie <kiarie7mbugua@gmail.com>
2024-02-15 22:44:14 +0300

    committed

---
+++ backend/server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Salazar!21",
+  database: "changa",
+  port: 3306,
+});
+
+const Port = 8080;
+
+app.get("/", (req, res) => {
+  return res.json("Hello this is the backend server");
+});
+
+app.get("/blogs", (req, res) => {
+  const blog = "SELECT * FROM blog_posts";
+  db.query(blog, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/create_blogs", (req, res) => {
+  const query =
+    "INSERT INTO blog_posts (`title`, `content`,`author`,)VALUES(?)";
+  const values = [
+    req.body.title,
+    req.body.content,
+    req.body.author,
+  ];
+
+  db.query(query, [values], (err, data) => {
+    if (err) return res.json(err);
+    res.status(201).send(data);
+    return res.json("blog has been created successfully");
+    
+  });
+});
+app.listen(Port, () => {
+  console.log(`Server is running on port ${Port}`);
+});
