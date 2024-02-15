3e4ee3e80fd0d16aa1608e31bf9e1b990c78001a
KingKiarie <kiarie7mbugua@gmail.com>
2024-02-15 23:40:06 +0300

    and the service that cometh

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AddBlogs from "./pages/AddBlogs.jsx";
+import DeleteBlogs from "./pages/DeleteBlogs.jsx";
+
+const paths = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/create_blog",
+    element: <AddBlogs />,
+  },
+  {
+    path: "/delete_blog",
+    element: <DeleteBlogs />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={paths} />
+  </React.StrictMode>
+);
---
+++ frontend/src/pages/DeleteBlogs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeleteBlogs = () => {
+  return <div>DeleteBlogs</div>;
+};
+
+export default DeleteBlogs;
